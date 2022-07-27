3b55adbbb32ad857a5d1cc74191b90e3a337510b
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-07-27 10:58:29 -0400

    Fix indentation

--- tests/rendering/incrementer-test.ts
+++ tests/rendering/incrementer-test.ts
@@ -1,23 +1,22 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, click } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
-            import { module, test } from 'qunit';
-            import { setupRenderingTest } from 'ember-qunit';
-            import { render, click } from '@ember/test-helpers';
-            import { hbs } from 'ember-cli-htmlbars';
+module('Rendering', function (hooks) {
+  setupRenderingTest(hooks);
 
-            module('Rendering', function (hooks) {
-              setupRenderingTest(hooks);
+  test('increments', async function (assert) {
+    await render(hbs`
+      <Incrementer as |count|>
+        <out>{{count}}</out>
+      </Incrementer>
+    `);
 
-              test('increments', async function (assert) {
-                await render(hbs`
-                  <Incrementer as |count|>
-                    <out>{{count}}</out>
-                  </Incrementer>
-                `);
+    assert.dom('out').hasText('0');
 
-                assert.dom('out').hasText('0');
+    await click('button');
+    assert.dom('out').hasText('1');
+  });
+});
 
-                await click('button');
-                assert.dom('out').hasText('1');
-              });
-            });
-          
