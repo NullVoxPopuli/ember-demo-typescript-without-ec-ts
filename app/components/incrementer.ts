d80a15d587183f5fb7a7cf6abf9bf37275afbcec
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-07-27 10:51:50 -0400

    Add a co-located component -- this is undetected

---
+++ app/components/incrementer.ts
@@ -0,0 +1,18 @@
+
+            import Component from '@glimmer/component';
+            import { tracked } from '@glimmer/tracking';
+            import { action } from '@ember/object';
+
+            interface Signature {
+              Element: HTMLDivElement;
+              Blocks: {
+                default: [number]
+              }
+            }
+
+            export default class Incrementer extends Component<Signature> {
+              @tracked count = 0;
+
+              @action increment() { this.count++ }
+            }
+          
---
+++ tests/rendering/incrementer-test.ts
@@ -0,0 +1,23 @@
+
+            import { module, test } from 'qunit';
+            import { setupRenderingTest } from 'ember-qunit';
+            import { render, click } from '@ember/test-helpers';
+            import { hbs } from 'ember-cli-htmlbars';
+
+            module('Rendering', function (hooks) {
+              setupRenderingTest(hooks);
+
+              test('increments', async function (assert) {
+                await render(hbs`
+                  <Incrementer as |count|>
+                    <out>{{count}}</out>
+                  </Incrementer>
+                `);
+
+                assert.dom('out').hasText('0');
+
+                await click('button');
+                assert.dom('out').hasText('1');
+              });
+            });
+          
